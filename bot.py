993be987b1756e6c15a6d76bac98c5f3253ce49d
acooper <an538926@dal.ca>
2021-11-17 12:45:43 -0400

    testing out commands & youtubedl.py

--- bot.py
+++ bot.py
@@ -1,15 +1,31 @@
 import discord
-import discord.ext.commands as commands
+import asyncio
+import requests
+import youtube_dl
+client = discord.Client()
 
-class myClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-    async def on_message(self, message):
-        if message.content.startswith('!hello'):
-            await self.send_message(message.channel, 'Hello!')
-client = myClient()
-client.run() #token goes here   
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    elif message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+    
+# voice channel commands for youtube bot 
+
+    elif message.content.startswith('!play'):
+        await message.channel.send('get lost nerdling !')
+        print(message.author.voice)
+        if message.author.voice != None: 
+            await message.author.move_to(None)
+        else:
+            await message.channel.send("You are not in a voice channel")
+
+
+client.run("OTEwMDE0OTQ3ODM5MzQwNjE0.YZMrdg.KIdXxml0AxqorHDmIgz5x6kYmhg")
+
